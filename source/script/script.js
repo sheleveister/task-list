09ceba12f0ac6c57ad2e66e76d2670688d040596
sheleveister <e.sheleveister@gmail.com>
2018-01-04 01:11:13 +0300

    refactor to es6 class

--- source/script/script.js
+++ source/script/script.js
@@ -1,80 +1,76 @@
 (function() {
   'use strict';
 
-  function TaskList() {
-    this.model = [
-      { text: 'Listen music' },
-      { text: 'Read book' },
-      { text: 'Go to walk' }
-    ];
+  class TaskList {
+    constructor() {
+      this.model = [
+        { text: 'Listen music' },
+        { text: 'Read books of Altshuller' },
+        { text: 'Learn React' },
+        { text: 'Feed the fish' },
+        { text: 'Read and reply to messages' },
+      ];
 
-    this.inputField = document.getElementsByClassName('task-form__text')[0];
-    this.form = document.getElementsByClassName('task-form')[0];
-    this.todoList = document.getElementsByClassName('table__body')[0];
-    this.btnTextRemove = 'X';
+      this.inputField = document.getElementsByClassName('task-form__text')[0];
+      this.form = document.getElementsByClassName('task-form')[0];
+      this.todoList = document.getElementsByClassName('table__body')[0];
+      this.btnTextRemove = 'X';
+    }
 
-    this.init();
+    getTaskNode(position, item) {
+      return `<tr>
+                <th>${position}</th>
+                <td>${item}</td>
+                <td><button type="button" class="btn btn-danger" id="${position}">${this.btnTextRemove}</button></td>
+              </tr>`;
+    }
+
+    renderList(model) {
+      let list = '';
+
+      model.map((item, i) => {
+        list += this.getTaskNode(i+1, item.text);
+      });
+
+      this.todoList.innerHTML = list;
+    }
+
+    onFormSubmit() {
+      if (this.inputField.value.length !== 0) {
+        this.addNewTask(this.inputField.value);
+      }
+      this.inputField.value = '';
+    }
+
+    addNewTask(value) {
+      let newTask = { text: value };
+      this.model.push(newTask);
+
+      this.renderList(this.model);
+    }
+
+    removeTask(id) {
+      this.model.splice(id - 1, 1);
+      this.renderList(this.model);
+    }
+
+    init() {
+      this.renderList(this.model);
+
+      this.form.addEventListener('submit', (event) => {
+        this.onFormSubmit(event);
+      });
+
+      this.todoList.addEventListener('click', (event) => {
+        if (event.target.id) {
+          this.removeTask(event.target.id);
+        }
+      });
+    }
+
   }
 
-  TaskList.prototype.getTaskNode = function(position, item) {
-
-    return `<tr>
-              <th>${position}</th>
-              <td>${item}</td>
-              <td><button type="button" class="btn btn-danger" id="${position}">${this.btnTextRemove}</button></td>
-            </tr>`;
-
-  };
-
-  TaskList.prototype.renderList = function(model) {
-    let list = '';
-
-    model.map((item, i) => {
-      list += this.getTaskNode(i+1, item.text);
-    });
-
-    this.todoList.innerHTML = list;
-  };
-
-  TaskList.prototype.onFormSubmit = function() {
-
-    if (this.inputField.value.length !== 0) {
-      this.addNewTask(this.inputField.value);
-    }
-    this.inputField.value = '';
-
-  };
-
-  TaskList.prototype.addNewTask = function(value) {
-
-    let newTask = { text: value };
-    this.model.push(newTask);
-
-    this.renderList(this.model);
-
-  };
-
-  TaskList.prototype.removeTask = function(id) {
-    this.model.splice(id - 1, 1);
-    this.renderList(this.model);
-  };
-
-  TaskList.prototype.init = function() {
-
-    this.renderList(this.model);
-
-    this.form.addEventListener('submit', (event) => {
-      this.onFormSubmit(event);
-    });
-
-    this.todoList.addEventListener('click', (event) => {
-      if (event.target.id) {
-        this.removeTask(event.target.id);
-      }
-    });
-
-  };
-
   const taskList = new TaskList();
+  taskList.init();
 
-}());
+})();
